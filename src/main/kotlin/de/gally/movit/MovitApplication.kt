0d790f02e5551183406fcc745401a6039c079aa3
Tioman Gally <tioman.gally@novatec-gmbh.de>
2020-04-20 12:54:04 +0200

    MoviT: Change docker image and tag name

--- src/main/kotlin/de/gally/movit/MovitApplication.kt
+++ src/main/kotlin/de/gally/movit/MovitApplication.kt
@@ -4,7 +4,10 @@ import de.gally.movit.exception.Message
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.router
 import reactor.kotlin.core.publisher.toMono
 
@@ -15,12 +18,19 @@ fun main(args: Array<String>) {
     runApplication<MovitApplication>(*args)
 }
 
-@Bean
-fun roomsRouter() = router {
-    accept(APPLICATION_JSON)
-            .nest {
-                GET("/alive") {
-                    ok().contentType(APPLICATION_JSON).body(Message("App is alive!").build().toMono(), String::class.java)
+@Suppress("UNUSED_PARAMETER")
+fun isAlive(request: ServerRequest) = ServerResponse.ok().contentType(APPLICATION_JSON).body(Message("App is alive!").build().toMono(), String::class.java)
+
+@Configuration
+class RouterService {
+
+    @Bean
+    fun roomsRouter() = router {
+        accept(APPLICATION_JSON)
+                .nest {
+                    "/alive".nest {
+                        GET("/", ::isAlive)
+                    }
                 }
-            }
+    }
 }
